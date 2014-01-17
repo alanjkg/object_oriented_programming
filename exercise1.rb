701b43919fb147affb03caa964d0abd7aa1814f9
alanjkg <alanjkg@gmail.com>
2014-01-17 01:39:01 -0500

    did ex2

--- exercise1.rb
+++ exercise1.rb
@@ -9,11 +9,6 @@ def greeting
 end
 end
 
-class Student < Person
-def learn
-	puts "I get it!"
-end
-end
 
 class Instructor < Person
 def teach
@@ -21,12 +16,15 @@ puts "Everything in Ruby is an Object."
 end
 end
 
-
+class Student < Person
+def learn
+	puts "I get it!"
+end
+end
 
 Chris = Instructor.new("Chris")
 Chris.greeting
 Chris.teach
-
 Christina = Student.new("Christina")
 Christina.greeting
 Christina.learn
